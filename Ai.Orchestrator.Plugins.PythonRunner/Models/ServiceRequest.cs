02fa80d5ac9c164d7e3cf4f02790df26b90f1bc7
smartguy05 <smartguy05@gmail.com>
2025-03-24 17:29:49 -0600

    plugin to run a python script

---
+++ Ai.Orchestrator.Plugins.PythonRunner/Models/ServiceRequest.cs
@@ -0,0 +1,11 @@
+﻿using Ai.Orchestrator.Models.Interfaces;
+
+namespace Ai.Orchestrator.Plugins.PythonRunner.Models;
+
+public class ServiceRequest: IPluginServiceRequest
+{
+    public string Method { get; set; }
+    public string ToolCallId { get; set; }
+    public string RequestingService { get; set; }
+    public string PythonScript { get; set; }
+}

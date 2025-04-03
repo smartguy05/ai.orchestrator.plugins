68d243c2479336c5a610d10b78d397a01ffec744
smartguy05 <smartguy05@gmail.com>
2025-04-03 09:18:12 -0600

    Home Assistant Assist plugin

---
+++ Ai.Orchestrator.Plugins.HomeAssistantAssist/HomeAssistantAssistCommand.cs
@@ -0,0 +1,48 @@
+﻿using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using Ai.Orchestrator.Models.Interfaces;
+using Ai.Orchestrator.Models.Tools;
+using Ai.Orchestrator.Plugins.HomeAssistantVoice.Models;
+
+namespace Ai.Orchestrator.Plugins.HomeAssistantVoice;
+
+public class HomeAssistantAssistCommand : CommandBase<ServiceRequest, ServiceConfig>
+{
+    public override string Name => "HomeAssist";
+    public override string Description => "A plugin to send natural language commands to Home Assistant";
+
+    public override async Task<object> DoWork(ServiceRequest serviceRequest, ServiceConfig config, IEnumerable<ToolCall> availableToolCalls)
+    {
+        using var httpClient = new HttpClient();
+        httpClient.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+        
+        if (!string.IsNullOrWhiteSpace(config.HomeAssistApiKey))
+        {
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", config.HomeAssistApiKey);
+        }
+
+        var body = new
+        {
+            text = serviceRequest.Query,
+            language = "en"
+        };
+        var jsonBody = JsonSerializer.Serialize(body);
+        var content = new StringContent(jsonBody, Encoding.UTF8, "application/json");
+
+        var response = await httpClient.PostAsync(config.HomeAssistUrl, content);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            Console.WriteLine("Unable to execute Home Assistant command");
+            Console.WriteLine(await response.Content.ReadAsStringAsync());
+            return new
+            {
+                Success = false
+            };
+        }
+
+        return await response.Content.ReadAsStringAsync();
+    }
+}
